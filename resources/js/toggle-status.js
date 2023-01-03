207433e9c072e991006272609fecd004747ed2b4
shisuiUfa <romanrus98@mail.ru>
2023-01-03 19:54:54 +0500

    Добавленно изменение состояния кнопки, по состоянию голосования

--- resources/js/toggle-status.js
+++ resources/js/toggle-status.js
@@ -1,7 +1,8 @@
 window.addEventListener('click', function (event) {
     if (event.target.classList.contains('btn-status')) {
         const voting = event.target.closest('tr'),
-            votingStatus = voting.querySelector('#status'),
+            votingStatus = voting.querySelector('.table__status'),
+            btnToggleStatus = voting.querySelector('.table__toggle-status'),
             votingId = voting.dataset.votingId;
         $.ajax({
             type: "POST",
@@ -12,8 +13,10 @@ window.addEventListener('click', function (event) {
                 let status;
                 if (response.close) {
                     status = "Закрыт";
+                    btnToggleStatus.innerHTML = `<i class="uil uil-unlock btn btn-status btn-primary"></i>`;
                 } else {
                     status = "Открыт";
+                    btnToggleStatus.innerHTML = `<i class="uil uil-lock btn btn-status btn-primary"></i>`;
                 }
                 votingStatus.innerHTML = `${status}`
             }
